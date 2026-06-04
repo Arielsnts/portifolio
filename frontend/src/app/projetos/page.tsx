af8ece83314028813b9a1bb108b7e0e714dfa09e
Ariel Santos <165219923+Arielsnts@users.noreply.github.com>
2026-06-04 11:31:15 -0300

    hotfix

--- frontend/src/app/projetos/page.tsx
+++ frontend/src/app/projetos/page.tsx
@@ -1,6 +1,9 @@
 import prisma from "@/lib/prisma"
 import styles from "./pageProjetos.module.css"
 
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
 type Projeto = {
    id: number
    nome: string
